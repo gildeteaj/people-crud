3b86c03e830f242e192de363e6d027cc83f98e30
Gildete <gildete.aj2@gmail.com>
2021-07-05 09:45:21 -0300

    -

--- src/main/java/com/stefanini/selecao/peoplews/config/WebConfig.java
+++ src/main/java/com/stefanini/selecao/peoplews/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.stefanini.selecao.peoplews.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -18,5 +20,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true);
+    }
+    
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
     }
 }
